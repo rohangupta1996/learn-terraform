0b1e69d01f90269216aed0c6ebc1987a0ae34c8b
Rohan <rgrohangupta65@gmail.com>
2023-06-29 15:58:08 +0530

    instance craetion

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "s1" {
   ami = "ami-03265a0778a880afb"
-  vpc_security_group_ids = {"sg-0c2a9ceb9bd234b68"}
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0c2a9ceb9bd234b68"]
   tags = {
     Name = "Test"
   }
