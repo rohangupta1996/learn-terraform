72f89ed3ef81bd94eaea1846fa0bd03a0cb3c8a7
Rohan <rgrohangupta65@gmail.com>
2023-07-07 22:39:08 +0530

    frontend in roboshop

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "s1" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0c2a9ceb9bd234b68"]
   tags = {
     Name = "Test"
   }
